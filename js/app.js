5133da159472d4f89be0ea37d96fcd342f04c88c
John Desrosiers <john.desro@gmail.com>
2020-12-21 13:48:40 -0800

    Extract code to fetch api data and create collection

--- js/app.js
+++ js/app.js
@@ -1,40 +1,4 @@
-async function fetchEntries() {
-  const url = new URL(
-    "https://raw.githubusercontent.com/public-apis/public-apis/master/README.md"
-  );
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      console.log(response.status, response.statusText);
-      return false;
-    }
-    // result is markdown for readme of the public-apis GitHub project
-    const result = await response.text();
-
-    // convert readme to html to easily parse out the rows of data from the numerous markdown tables
-    const page = document.createElement("div");
-    const md = new remarkable.Remarkable();
-    page.innerHTML = md.render(result);
-    const rows = page.querySelectorAll("tbody tr");
-    const entries = Array.from(rows).map((row) => {
-      const tds = row.children;
-      const entry = {
-        title: tds[0].textContent,
-        url: tds[0].firstElementChild.getAttribute("href"),
-        description: tds[1].textContent,
-        auth: tds[2].textContent,
-        https: tds[3].textContent,
-        cors: tds[4].textContent,
-        row,
-      };
-      return entry;
-    });
-    return entries;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-}
+import { fetchApiCollection } from "./api_collection.js";
 
 function renderTable(entries) {
   const table = document.createElement("table");
@@ -57,12 +21,10 @@ function renderTable(entries) {
 }
 
 async function main() {
-  const entries = await fetchEntries();
-  if (entries) {
-    const corsAndHttpsEntries = entries.filter(
-      (entry) => entry.cors === "Yes" && entry.https === "Yes"
-    );
-    renderTable(corsAndHttpsEntries);
+  const collection = await fetchApiCollection();
+  if (collection) {
+    const entries = collection.all();
+    renderTable(entries);
   } else {
     document.getElementById("error-view").classList.remove("d-none");
   }
