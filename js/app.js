498bd4f605c6a83219554aca5aa6912636ff4cb6
John Desrosiers <john.desro@gmail.com>
2020-12-24 06:11:31 -0800

    Refactor to extract table component

--- js/app.js
+++ js/app.js
@@ -1,98 +1,53 @@
 import { fetchApiCollection } from "./api_collection.js";
-import { biStar, biStarFill } from "./icons.js";
-
-function renderTable({ entries, tableView }) {
-  const table = document.createElement("table");
-  table.classList.add("table");
-  table.innerHTML = `
-    <thead>
-      <tr>
-        <th></th>
-        <th>API</th>
-        <th>Category</th>
-        <th>Description</th>
-        <th>Auth</th>
-        <th>HTTPS</th>
-        <th>CORS</th>
-      </tr>
-    </thead>`;
-
-  const tbody = document.createElement("tbody");
-  const rows = entries.map((entry) => {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>
-        <span data-entryTitle="${entry.title}" class="favorite">
-          ${entry.isFavorite ? biStarFill() : biStar()}
-        </span>
-      </td>
-      <td><a href="${entry.url}">${entry.title}</a></td>
-      <td>${entry.category}</td>
-      <td>${entry.description}</td>
-      <td>${entry.auth}</td>
-      <td>${entry.https}</td>
-      <td>${entry.cors}</td>
-    `;
-    return tr;
-  });
-  tbody.append(...rows);
-  table.append(tbody);
-  tableView.innerHTML = "";
-  tableView.append(table);
-}
+import {
+  enableTableControls,
+  getTableState,
+  initTable,
+  setTableEntries,
+  showTable,
+} from "./table.js";
 
 async function main() {
   const pageLoadAt = Date.now();
-  const corsToggle = document.querySelector("#cors-toggle");
   const errorView = document.querySelector("#error-view");
-  const favoritesToggle = document.querySelector("#favorites-toggle");
-  const httpsToggle = document.querySelector("#https-toggle");
   const loader = document.querySelector("#loader");
-  const searchInput = document.querySelector("#search-input");
-  const tableButtons = document.querySelector("#table-buttons");
-  const tableView = document.getElementById("table-view");
-  const filterEntries = () => {
-    return collection.find({
-      cors: corsToggle.checked,
-      isFavorite: favoritesToggle.checked,
-      https: httpsToggle.checked,
-      search: searchInput.value.trim(),
-    });
-  };
+
   const collection = await fetchApiCollection();
+
+  const filterEntries = () => {
+    const { cors, favorites, https, search } = getTableState();
+    const entries = collection.find({
+      cors,
+      https,
+      isFavorite: favorites,
+      search,
+    });
+    setTableEntries(entries);
+  };
+
   if (collection) {
     const entries = collection.all();
-    renderTable({ tableView, entries });
 
     // use timeout to prevent flash of loader which appears as a bug to the
     // observant user
     const MIN_LOADER_TIME = 800;
     setTimeout(() => {
-      tableView.classList.remove("d-none");
       loader.classList.add("d-none");
-      corsToggle.disabled = false;
-      favoritesToggle.disabled = false;
-      httpsToggle.disabled = false;
-      searchInput.disabled = false;
-      // use parens due to 1000 - Date.now() producing unsafe number
+      enableTableControls();
+      showTable();
+      // use parens due to MIN_LOADER_TIME - Date.now() producing unsafe number
     }, MIN_LOADER_TIME - (Date.now() - pageLoadAt));
 
-    tableButtons.addEventListener("change", () => {
-      renderTable({ tableView, entries: filterEntries() });
-    });
-    searchInput.addEventListener("input", () => {
-      renderTable({ tableView, entries: filterEntries() });
-    });
-
-    const handleFavoriteClick = (event) => {
-      const favoriteStar = event.target.closest(".favorite");
-      if (favoriteStar) {
-        const entryTitle = favoriteStar.getAttribute("data-entryTitle");
-        collection.toggleFavorite(entryTitle);
-        renderTable({ tableView, entries: filterEntries() });
-      }
+    const handleFavoriteChange = (entryTitle) => {
+      collection.toggleFavorite(entryTitle);
+      filterEntries();
     };
-    document.addEventListener("click", handleFavoriteClick);
+
+    initTable({
+      entries,
+      onChange: filterEntries,
+      onFavoriteChange: handleFavoriteChange,
+    });
   } else {
     errorView.classList.remove("d-none");
   }
---
+++ js/table.js
@@ -0,0 +1,93 @@
+import { biStar, biStarFill } from "./icons.js";
+
+let corsToggle;
+let favoritesToggle;
+let httpsToggle;
+let searchInput;
+let tableButtons;
+let tableView;
+
+const headerHTML = () => {
+  return `
+  <thead>
+    <tr>
+      <th></th>
+      <th>API</th>
+      <th>Category</th>
+      <th>Description</th>
+      <th>Auth</th>
+      <th>HTTPS</th>
+      <th>CORS</th>
+    </tr>
+  </thead>`;
+};
+
+const rowHTML = (entry) => {
+  return `
+    <tr>
+      <td>
+        <span data-entryTitle="${entry.title}" class="favorite">
+          ${entry.isFavorite ? biStarFill() : biStar()}
+        </span>
+      </td>
+      <td><a href="${entry.url}">${entry.title}</a></td>
+      <td>${entry.category}</td>
+      <td>${entry.description}</td>
+      <td>${entry.auth}</td>
+      <td>${entry.https}</td>
+      <td>${entry.cors}</td>
+    </tr>
+  `;
+};
+
+const bodyHTML = (entries) => `<tbody>${entries.map(rowHTML).join("")}</tbody>`;
+
+const renderTable = ({ entries, tableView }) => {
+  const table = document.createElement("table");
+  table.classList.add("table");
+  table.innerHTML = headerHTML() + bodyHTML(entries);
+  tableView.innerHTML = "";
+  tableView.append(table);
+};
+
+const getTableState = () => ({
+  cors: corsToggle.checked,
+  favorites: favoritesToggle.checked,
+  https: httpsToggle.checked,
+  search: searchInput.value.trim(),
+});
+
+const initTable = ({ entries, onFavoriteChange, onChange }) => {
+  corsToggle = document.querySelector("#cors-toggle");
+  favoritesToggle = document.querySelector("#favorites-toggle");
+  httpsToggle = document.querySelector("#https-toggle");
+  searchInput = document.querySelector("#search-input");
+  tableButtons = document.querySelector("#table-buttons");
+  tableView = document.getElementById("table-view");
+
+  tableButtons.addEventListener("change", onChange);
+  searchInput.addEventListener("input", onChange);
+  document.addEventListener("click", (event) => {
+    const favoriteStar = event.target.closest(".favorite");
+    if (favoriteStar) {
+      const entryTitle = favoriteStar.getAttribute("data-entryTitle");
+      onFavoriteChange(entryTitle);
+    }
+  });
+  renderTable({ tableView, entries });
+};
+
+const setTableEntries = (entries) => {
+  renderTable({ tableView, entries });
+};
+
+const enableTableControls = () => {
+  corsToggle.disabled = false;
+  favoritesToggle.disabled = false;
+  httpsToggle.disabled = false;
+  searchInput.disabled = false;
+};
+
+const showTable = () => tableView.classList.remove("d-none");
+
+export { initTable, setTableEntries, getTableState, enableTableControls, showTable };
