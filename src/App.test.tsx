7783e82c4042d1653c7e006d61d6414a3516364f
John Desrosiers <john.desro@gmail.com>
2022-08-18 06:06:28 -0700

    Fix App.test warning

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,18 +11,12 @@ jest.mock("./ApiContainer", () => () => <MockComponent />);
 test("renders App with a loader until ApiContainer lazy loads", async () => {
   render(<App />);
 
-  // recall that findBy* and getBy* will throw if nothing matches or > 1 match
-  // wrapped in await act call to silence warning from react test util
-  // eslint-disable-next-line testing-library/no-unnecessary-act
-  await act(async () => {
-    await screen.findByRole("heading", {
-      name: /search for public apis/i,
-    });
-    // assert that accessible loader is rendered
-    const loader = screen.getByRole("status");
-    expect(loader.textContent).toMatch(/searching for apis/i);
+  await screen.findByRole("heading", {
+    name: /search for public apis/i,
   });
-
+  // assert that accessible loader is rendered
+  const loader = screen.getByRole("status");
+  expect(loader.textContent).toMatch(/searching for apis/i);
 
   // wait for lazy loaded component
   await screen.findByText(/mock/i);
